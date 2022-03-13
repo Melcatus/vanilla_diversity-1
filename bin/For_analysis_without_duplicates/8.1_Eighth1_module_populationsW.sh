a301936dcfee438390aa8f1227ccb7cbd3834d49
gutierrezalejoa@comunidad.unam.mx <gutierrezalejoa@comunidad.unam.mx>
2022-03-13 00:02:58 -0600

    Added modules 7 and 8 for just wild/cultivated analysis

---
+++ bin/For_analysis_without_duplicates/8.1_Eighth1_module_populationsW.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+####For analysis population in the last output files with stacks
+##Prerequisites, Stacks populations module
+##Make a module for output
+mkdir ../Process/data/popf
+mkdir ../Process/data/popf/second_an
+mkdir ../Process/data/popf/second_an/wild
+
+##To do analysis on the last output of gstacks (seventh module)
+##The outputs on this analysis was determined for software 'STRUCTURE', 'GENEPOP', 'Phylip' and 'VCFtools'
+stacks populations -P ../Process/data/gwens/second_an/wild -O ../Process/data/popf/second_an/wild -M ../Process/data/"pmap_nodW.tsv" --min-samples-per-pop 0.01 --min-samples-overall 0.01 --min-population 1 --hwe --fstats --ordered-export --fasta-loci --fasta-samples --vcf
---
+++ bin/For_analysis_without_duplicates/8.2_Eighth.2_module_populationsC.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+####For analysis population in the last output files with stacks
+##Prerequisites, Stacks populations module
+##Make a module for output
+mkdir ../Process/data/popf
+mkdir ../Process/data/popf/second_an
+mkdir ../Process/data/popf/second_an/culti
+
+##To do analysis on the last output of gstacks (seventh module)
+##The outputs on this analysis was determined for software 'STRUCTURE', 'GENEPOP', 'Phylip' and 'VCFtools'
+stacks populations -P ../Process/data/gwens/second_an/culti -O ../Process/data/popf/second_an/culti -M ../Process/data/"pmap_nodC.tsv" --min-samples-per-pop 0.01 --min-samples-overall 0.01 --min-population 1 --hwe --fstats --ordered-export --fasta-loci --fasta-samples --vcf
